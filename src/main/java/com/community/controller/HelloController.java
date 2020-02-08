304e51b64524b984ce04afb1993712a5bd6c8adb
noissues <noissues@users.noreply.github.com>
2020-02-08 16:40:30 +0800

    init repo

---
+++ src/main/java/com/community/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller // 告诉 Spring 这是一个 Controller
+public class HelloController {
+    @GetMapping("/hello") // 确保对 /hello 的 Get 请求映射到 hello() 方法上
+    public String hello(@RequestParam(name = "name", defaultValue = "root") String name,
+                        Model model) {
+        model.addAttribute("name", name); // 将 name 通过 Model 传出去
+        return "hello";   // 返回名为 hello 的视图
+    }
+}
